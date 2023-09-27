ce566817164aadb59c13e879a2352d94fc9c6f65
osiris <yfnf72@gmail.com>
2023-09-27 16:52:13 +0300

    Initial commit

---
+++ src/main/java/com/example/yandexlavkarefactored/YandexLavkaRefactoredApplication.java
@@ -0,0 +1,13 @@
+package com.example.yandexlavkarefactored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexLavkaRefactoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexLavkaRefactoredApplication.class, args);
+	}
+
+}
